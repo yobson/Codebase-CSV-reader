117a4db335f6c42e5c15bbed50b4344fe8ab4231
jameshobson <jameshobson@DESKTOP-0E6THK4>
2016-08-31 16:54:35 +0100

    Added Colour

--- Bug Reviewer/Program.fs
+++ Bug Reviewer/Program.fs
@@ -25,16 +25,28 @@ let removeSpaces (a:string) =
     second.Split ([|"."|], System.StringSplitOptions.None)
     |> System.String.Concat
 
+let writeInColour (text : string) (colour : System.ConsoleColor) =
+    System.Console.ForegroundColor <- colour
+    System.Console.Write text
+    System.Console.ResetColor()
+
 [<EntryPoint>]
 let main argv =
 
     let ticket = argv |> Seq.head |> System.IO.Path.GetFileName |> removeSpaces |> splitName
     let bugList = Bugs.Load(argv |> Seq.head)
-    printfn "Hit return to open next page"
+    printfn "Hit return to open next page\n"
     for row in bugList.Rows |> Seq.tail do
         openURLInBrowser ("http://codebase.enjoy-digital.co.uk/projects/" + ticket + "/tickets/" + row.``#``.ToString())
         |> ignore
-        printfn "Ticket is a %s and is %s in urgancy.\n It's current status is %s" row.Type row.Priority row.Status
+        //printfn "Ticket is a %s and is %s in urgancy.\n It's current status is %s" row.Type row.Priority row.Status
+        writeInColour "\nTicket is a " System.ConsoleColor.White
+        writeInColour row.Type System.ConsoleColor.Red
+        writeInColour " and is " System.ConsoleColor.White
+        writeInColour row.Priority System.ConsoleColor.Red
+        writeInColour " in urgancy. \nIt's current status is " System.ConsoleColor.White
+        writeInColour row.Status System.ConsoleColor.Red
+
         System.Console.ReadLine()
         |> ignore
     0

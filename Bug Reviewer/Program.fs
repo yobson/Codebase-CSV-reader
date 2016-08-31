b6ccfe05ca75127e7ef1a568632de8e4479894a2
jameshobson <jameshobson@DESKTOP-0E6THK4>
2016-08-31 13:46:01 +0100

    Built App

--- Bug Reviewer/Program.fs
+++ Bug Reviewer/Program.fs
@@ -1,7 +1,41 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open FSharp.Data
+
+type Bugs =  CsvProvider<"csv.csv">
+
+let openURLInBrowser (url:string) =
+        System.Diagnostics.Process.Start(url)
+
+
+let splitName (text:string) =
+    text.Split ([|"-All"|], System.StringSplitOptions.None)
+    |> Seq.head 
+
+let removeSpaces (a:string) =
+    let first = 
+        a.Split ([|" "|], System.StringSplitOptions.None)
+        |> Seq.map(fun x -> x + "-")
+        |> System.String.Concat
+    let second =
+        first.Split ([|"---"|], System.StringSplitOptions.None)
+        |> Seq.map(fun x -> x + "-")
+        |> System.String.Concat
+    second.Split ([|"."|], System.StringSplitOptions.None)
+    |> System.String.Concat
+
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+
+    let ticket = argv |> Seq.head |> System.IO.Path.GetFileName |> removeSpaces |> splitName
+    let bugList = Bugs.Load(argv |> Seq.head)
+    printfn "Hit return to open next page"
+    for row in bugList.Rows |> Seq.tail do
+        openURLInBrowser ("http://codebase.enjoy-digital.co.uk/projects/" + ticket + "/tickets/" + row.``#``.ToString())
+        |> ignore
+        System.Console.ReadLine()
+        |> ignore
+    0
+
+
